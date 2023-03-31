a8375f897a6d1b443c7a24c6e55d42803c9bf0ee
douglas <schonholtzd@gmail.com>
2023-03-30 23:42:54 -0400

    Bash agent before latest refactor

--- BashAgent.py
+++ BashAgent.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 import queue
 import json
 from langchain.agents import Tool
@@ -9,10 +8,14 @@ from langchain.llms import OpenAI
 from langchain.utilities import SerpAPIWrapper, BashProcess
 from langchain.agents import initialize_agent, load_tools
 
+from langchain.prompts.chat import (
+    SystemMessagePromptTemplate,
+)
+
 os.environ["LANGCHAIN_HANDLER"] = "langchain"
 
 
-def create_agent(agent_type):
+def create_agent(agent_type, task):
     search = SerpAPIWrapper()
     bash = BashProcess()
     llm = ChatOpenAI(temperature=0)
@@ -37,7 +40,13 @@ def create_agent(agent_type):
         ])
 
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    agent_chain = initialize_agent(tools, llm, agent=agent_type, verbose=True, memory=memory)
+    agent_chain = initialize_agent(
+        tools,
+        llm,
+        agent=agent_type,
+        verbose=True,
+        memory=memory,
+        agent_kwargs={"system_message": f"You are an agent who must do the following: {task}."})
 
     return agent_chain
 
@@ -49,13 +58,7 @@ def process_task_queue(task_queue, task_executor_agent, task_creation_agent):
         result = task_executor_agent.run(input=task)
         print(result)
 
-        custom_prompt = f"Parse the output of the execution agent which had prompt: <Start>{task}<end>" \
-                        f"\nIt generated the following <start>{result}<end>\n" \
-                        f"If the output is correct, return a json object with the key 'prompt' " \
-                        f"and the value 'None'.\n" \
-                        f"If the output is incorrect or the task isn't completed in it's entirety, " \
-                        f"return a json object with the key 'prompt' and the value of the" \
-                        f" new prompt which should help finish the task."
+        custom_prompt = f"{task} {result}"
         new_task_info = task_creation_agent.run(input=custom_prompt)
         new_task_info = json.loads(new_task_info)
         if new_task_info["prompt"]:
@@ -65,17 +68,42 @@ def process_task_queue(task_queue, task_executor_agent, task_creation_agent):
 
 
 def main():
-    task_executor_agent = create_agent("chat-conversational-react-description")
-    task_creation_agent = ChatOpenAI(temperature=0)
 
     task_queue = queue.Queue()
-
+    print('started')
     while True:
         task = input("Please provide a task (or type 'exit' to quit): ")
         if task.lower() == "exit":
             break
+        task_queue.put("Define what your first task should be.")
+        task_executor_agent = create_agent("chat-conversational-react-description", task)
+        task_creation_llm = ChatOpenAI(temperature=0)
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-        task_queue.put(task)
+        task_creation_prompt = f"You are an agent responsible for creating new sub tasks to fulfill the long term " \
+                               f"goal: {task}" \
+                                f"You will create new tasks to help fulfill that goal." \
+                                f"You will get inputs of the format: " \
+                                f"{{ A long task description}} " \
+                                f"{{ A long result from attempting to complete that task }}" \
+                                f'If this doesn\'t appear to complete the original task described above. ' \
+                                f'Create a new task by returning the prompt to be given to a language model to' \
+                                f' complete the next step in completing the main task.' \
+                                f'To do this, return a json object with key prompt and value None if no new tasks ' \
+                                f'need to be created OR with the prompt for the next task to be done if new tasks ' \
+                                f'do need to be completed.' \
+                                f'Example json object if the task was completed: {{"prompt": None}}' \
+                                f'Example json object if the task wasn\'t completed: ' \
+                                f'{{"prompt": "new prompt to help complete the task}}"'
+
+        task_creation_agent = initialize_agent(
+            [],
+            task_creation_llm,
+            agent="chat-conversational-react-description",
+            verbose=True,
+            memory=memory,
+            agent_kwargs={"system_message": f"You are an agent who must do the following: {task}."}
+        )
         process_task_queue(task_queue, task_executor_agent, task_creation_agent)
 
 
